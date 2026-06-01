3d5f45b39b32e8454c3874387b03dcdbaf1968ed
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-06-01 20:34:02 +0530

    revert to pure service:gmail for nodemailer

--- backend/server.js
+++ backend/server.js
@@ -82,19 +82,10 @@ const Setting = mongoose.model("Setting", settingSchema);
 ========================= */
 
 const transporter = nodemailer.createTransport({
-    host: "192.178.211.108", // Used IP explicitly instead of smtp.gmail.com
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    // Adding extra timeouts
-    connectionTimeout: 10000, 
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    tls: {
-       rejectUnauthorized: false
     }
 });
 /* =========================
