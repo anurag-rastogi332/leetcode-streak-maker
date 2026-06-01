d3f9a326916bea10b809ee8d20ad7a6fe2d01e5c
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-06-01 20:27:48 +0530

    switch to port 587 for nodemailer

--- backend/server.js
+++ backend/server.js
@@ -83,8 +83,8 @@ const Setting = mongoose.model("Setting", settingSchema);
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, 
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
