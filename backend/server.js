9d6b9c3d36f19624b7c0a08a38faaa20f51e72e4
anurag-rastogi332 <rastogianurag675@gmail.com>
2026-06-04 23:58:57 +0530

    Describe your changes

--- backend/server.js
+++ backend/server.js
@@ -81,11 +81,24 @@ const Setting = mongoose.model("Setting", settingSchema);
    Email Transporter
 ========================= */
 
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     }
+// });
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
